fa1cc48aa158497d40d7f435514901016a60c6a2
0xb <mbsgzz@126.com>
2022-07-29 00:13:54 +0800

    chore: add element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus, { size: 'small', locale: zhCn })
+app.mount('#app')
