12cb5435362e8a59a4762161675abcdce47e4e67
0xb <mbsgzz@126.com>
2022-08-06 15:52:52 +0800

    feat: 拆分路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,40 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    component: () => import('../views/json-to-table.vue'),
-    name: 'json-to-table',
-    path: '/json-to-table'
-  },
-  {
-    component: () => import('../views/time-data-chart.vue'),
-    name: 'time-data-chart',
-    path: '/time-data-chart'
-  },
-  {
-    component: () => import('../views/hex-ieee754.vue'),
-    name: 'hex-ieee754',
-    path: '/hex-ieee754'
-  }
-]
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes as routeItems } from './routes';
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes: routeItems
+});
 
-export default router
+export const routes = routeItems;
+
+export default router;
---
+++ src/router/routes.ts
@@ -0,0 +1,36 @@
+
+import { RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    component: () => import('../views/json-to-table.vue'),
+    name: 'json-to-table',
+    path: '/json-to-table'
+  },
+  {
+    component: () => import('../views/time-data-chart.vue'),
+    name: 'time-data-chart',
+    path: '/time-data-chart'
+  },
+  {
+    component: () => import('../views/hex-ieee754.vue'),
+    name: 'hex-ieee754',
+    path: '/hex-ieee754'
+  }
+];
+
+export default routes;
